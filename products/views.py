e24221ee0e435fea3e95d5b12dadaf4168940401
sami-sinnari <samisinnari@gmail.com>
2021-08-12 19:53:52 +0000

    Bag app created and set-up

--- products/views.py
+++ products/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
-from .models import Product
+
+from .models import Product, Category
+
+# Create your views here.
+
 
 # Create your views here.
 
 def all_products(request):
+    """ A view to show all products, including sorting and search queries """
 
     products = Product.objects.all()
     query = None
@@ -22,7 +27,7 @@ def all_products(request):
             if not query:
                 messages.error(request, "You didn't enter any search criteria!")
                 return redirect(reverse('products'))
-            
+
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
 
@@ -33,8 +38,6 @@ def all_products(request):
     }
 
     return render(request, 'products/products.html', context)
-
-
 
 
 def product_detail(request, product_id):
