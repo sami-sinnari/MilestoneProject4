3a5c0646d56eb345a1b64482f83f1723a8a3dc25
sami-sinnari <samisinnari@gmail.com>
2021-08-13 18:46:37 +0000

    Added view for removing the item from the bag

--- bag/views.py
+++ bag/views.py
@@ -48,3 +48,16 @@ def adjust_bag(request, item_id):
 
     request.session['bag'] = bag
     return redirect(reverse('view_bag'))
+
+
+def remove_from_bag(request, item_id):
+    """Remove the item from the shopping bag"""
+
+    product = get_object_or_404(Product, pk=item_id)
+    bag = request.session.get('bag', {})
+
+    bag.pop(item_id)
+    messages.success(request, f"Removed '{product.name}' from your bag")
+
+    request.session['bag'] = bag
+    return HttpResponse(status=200)
