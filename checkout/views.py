2705398be1c067ed7f7aefdce2e24fac9708f9bd
sami-sinnari <samisinnari@gmail.com>
2021-08-14 19:46:28 +0000

    Started Stripe connection in html files and views

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51JOShfCvZQZwuzSFxFXQujSg7bLukmwauMvvmARSwBbEDrhOcYDKeRHf9hJx5wJv8GEHm45PaSKRsSLjm0MhNyVP00raxbkXX5',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
