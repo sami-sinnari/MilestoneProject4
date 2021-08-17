b1f10c77496f7d6f3e190d62b041d93623c15660
sami-sinnari <samisinnari@gmail.com>
2021-08-17 17:41:00 +0000

    Updated success messages for contact

--- home/views.py
+++ home/views.py
@@ -6,7 +6,3 @@ def index(request):
     """ A view to return the index page """
 
     return render(request, 'home/index.html')
-
-
-def contact(request):
-    return render(request, '/contact.html')
--- sendemail/views.py
+++ sendemail/views.py
@@ -1,9 +1,22 @@
-from django.core.mail import send_mail, BadHeaderError
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from .forms import ContactForm
+from profiles.models import UserProfile
+
 
 def contactView(request):
+
+    if request.user.is_authenticated:
+        try:
+            profile = UserProfile.objects.get(user=request.user)
+            form = ContactForm(initial={
+                'email': profile.user.email,
+            })
+        except UserProfile.DoesNotExist:
+            form = ContactForm()
+    else:
+        form = ContactForm()
+
     if request.method == 'GET':
         form = ContactForm()
     else:
@@ -13,17 +26,17 @@ def contactView(request):
             from_email = form.cleaned_data['from_email']
             message = form.cleaned_data['message']
             try:
-                send_mail(subject, message, from_email, ['samisinnari@gmail.com'])
+                send_mail(subject, message, from_email,
+                          ['samisinnari@gmail.com'])
             except BadHeaderError:
                 return HttpResponse('Invalid header found.')
             return redirect('success')
-
     context = {
         'contact_page': 'active',
         'form': form,
     }
-
     return render(request, "sendemail/contact.html", context)
+
 
 def successView(request):
 
