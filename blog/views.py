54f5dc9c65cdb66efe0684d82667682918a016f9
sami-sinnari <samisinnari@gmail.com>
2021-08-17 17:36:12 +0000

    Changed ot 2 posts per page on blog

--- blog/views.py
+++ blog/views.py
@@ -7,12 +7,12 @@ from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 
 def all_posts(request):
     post_list = Post.objects.filter(status=1).order_by('-created_on')
-    paginator = Paginator(post_list, 3)  # 3 posts in each page
+    paginator = Paginator(post_list, 2)  # 2 posts in each page
     page = request.GET.get('page')
     try:
         post_list = paginator.page(page)
     except PageNotAnInteger:
-            # If page is not an integer deliver the first page
+        # If page is not an integer deliver the first page
         post_list = paginator.page(1)
     except EmptyPage:
         # If page is out of range deliver last page of results
