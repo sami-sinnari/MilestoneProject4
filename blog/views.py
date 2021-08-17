333ee3459d0966fd25e95fd7a2cd49ffee40eaf4
sami-sinnari <samisinnari@gmail.com>
2021-08-17 15:35:38 +0000

    Added blog forms and corrected views

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,17 @@
+from django.shortcuts import render
 from django.views import generic
 from .models import Post
+
+
+def all_posts(request):
+    post_list = Post.objects.filter(status=1)
+    template = "blog/blog.html"
+    context = {
+        'post_list': post_list
+    }
+    return render(request, template, context)
+
 
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1).order_by('-created_on')
     template_name = 'blog.html'
-
-class PostDetail(generic.DetailView):
-    model = Post
-    template_name = 'post_detail.html'
