b3b5dbb7065848415a046de3b75f4377c5bec5d9
sami-sinnari <samisinnari@gmail.com>
2021-08-17 16:23:54 +0000

    Setup send email fucntionality and changed template name

--- home/views.py
+++ home/views.py
@@ -6,3 +6,7 @@ def index(request):
     """ A view to return the index page """
 
     return render(request, 'home/index.html')
+
+
+def contact(request):
+    return render(request, '/contact.html')
