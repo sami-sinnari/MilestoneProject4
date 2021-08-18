ccc72b4fa74c59ebaca8bda1efae97c7a0ed77d0
sami-sinnari <samisinnari@gmail.com>
2021-08-18 22:15:23 +0000

    Connected faq with home and added css for it

--- home/views.py
+++ home/views.py
@@ -6,3 +6,12 @@ def index(request):
     """ A view to return the index page """
 
     return render(request, 'home/index.html')
+
+
+def accordion(request):
+
+    context = {
+        'accordion_page': 'active',
+    }
+
+    return render(request, 'home/accordion.html', context)
